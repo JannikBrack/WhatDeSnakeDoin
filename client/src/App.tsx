b6ea1726cd3341c374ea0cd7346c54c217d2dd52
jannik brack <jannik.brack@wheregroup.com>
2025-03-17 16:31:46 +0100

    add routes with no topbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ import {Box, Typography} from '@mui/material';
 import ToolBar from "./components/ToolBar";
 import appUrl from "./lib/appUrl";
 import SnakeViewerComponent from "./components/SnakeViewerComponent";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
+import DeleteSnakeFormComponent from "./components/DeleteSnakeFormComponent";
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
         width: '100%',
         height: '100%',
     }
+    const location = useLocation();
+
+    const hideToolBar = ["/deletesnake"]
 
     return (
         <>
@@ -32,46 +36,65 @@ function App() {
                     color: 'white',
                 }}
             >
-                <ToolBar/>
-                    <Routes>
-                        <Route path="/" element={
-                            <>
-                                {/*MainMenu*/}
-                                <Box
-                                    sx={{
-                                        ...mainBoxConfig,
-                                        backgroundImage: `url(${appUrl}images/MainMenu.png)`,
-                                    }}
-                                >
-                                    <Typography
-                                        variant="h4"
-                                        sx={{
-                                            position: 'absolute',
-                                            top: 600,
-                                            p: 2,
-                                            textAlign: 'center',
-                                            color: 'white',
-                                            borderRadius: 2,
-                                        }}
-                                    >
-                                        WhatDeSnakeDoin?
-                                    </Typography>
-                                </Box>
-                            </>
-                        }/>
-
-                        {/*SnakeSide*/}
-                        <Route path="/snake" element={
+                {!hideToolBar.includes(location.pathname) && <ToolBar/>}
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            {/*MainMenu*/}
                             <Box
                                 sx={{
                                     ...mainBoxConfig,
-                                    backgroundImage: `url(${appUrl}images/Background.png)`,
+                                    backgroundImage: `url(${appUrl}images/MainMenu.png)`,
                                 }}
                             >
-                                <SnakeViewerComponent/>
+                                <Typography
+                                    variant="h4"
+                                    sx={{
+                                        position: 'absolute',
+                                        top: 600,
+                                        p: 2,
+                                        textAlign: 'center',
+                                        color: 'white',
+                                        borderRadius: 2,
+                                    }}
+                                >
+                                    WhatDeSnakeDoin?
+                                </Typography>
                             </Box>
-                        }/>
-                    </Routes>
+                        </>
+                    }/>
+
+                    {/*SnakeSide*/}
+                    <Route path="/snake" element={
+                        <Box
+                            sx={{
+                                ...mainBoxConfig,
+                                backgroundImage: `url(${appUrl}images/Background.png)`,
+                            }}
+                        >
+                            <SnakeViewerComponent/>
+                        </Box>
+                    }/>
+
+
+
+                    {/*paths without Toolbar*/}
+                    <Route path="/deletesnake" element={
+                        <Box
+                            sx={{
+                                height: '100%',
+                                widht: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'space-evenly',
+                                backgroundColor: '#45304c',
+                            }}
+                        >
+                            <DeleteSnakeFormComponent/>
+                        </Box>
+                    }/>
+                </Routes>
             </Box>
         </>
     );
